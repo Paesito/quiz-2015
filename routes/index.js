4e29dd22eefcc05b719a5a36bd0e12ea808fa52f
Paesito <pae_sb19@hotmail.com>
2015-08-09 14:56:08 +0200

    Primera Pregunta
    se realiza la primera pregunta del juego

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// Importamos el modulo quiz_controller 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+
+// Importamos dos nuevas rutas haciendo un GET ejecutando los controladores de quiz_controller
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer' , quizController.answer);
 
 module.exports = router;
